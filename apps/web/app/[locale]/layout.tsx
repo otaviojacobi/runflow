0f1ce1531340cb097ae339af3946f2f5c8d5939f
Otavio Jacobi <otaviojacobi@gmail.com>
2025-10-07 13:49:38 -0300

    speed insights

--- apps/web/app/[locale]/layout.tsx
+++ apps/web/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "../globals.css";
 
 const geistSans = localFont({
@@ -41,6 +42,7 @@ export default async function LocaleLayout({
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
